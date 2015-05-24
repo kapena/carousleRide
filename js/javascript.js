1c5f9e1ec59caf927db518e67172a9b6f8e3e295
Brent <brentw.white@gmail.com>
2015-05-23 16:32:28 -1000

    variables added

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+//  transitionSpeed of the images interchanging
+var transitionSpeed = 5000;
+// finding our images with the children method
+var itemList = $('ul').children('li');
+// finding our pagination links with the children method
+var pagination = $('.pagination-carousel').children('a');
+// setting itemList to the length property which is allowing us to search through each of our images.
+var lengthOfImages = itemList.length;
+// set the current postion where we want to start searching
+var current = 0;
+
+});
