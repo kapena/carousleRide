e251192500096fcbb86d37cce3790fdda167e23a
Brent <brentw.white@gmail.com>
2015-05-27 11:43:14 -1000

    script markdown changes

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,10 @@
 $(document).ready(function(){
-//  transitionSpeed of the images interchanging
-var transitionSpeed = 5000;
-// finding our images with the children method
-var itemList = $('ul').children('li');
-// finding our pagination links with the children method
-var pagination = $('.pagination-carousel').children('a');
-// setting itemList to the length property which is allowing us to search through each of our images.
-var lengthOfImages = itemList.length;
-// set the current postion where we want to start searching
-var current = 0;
+    // click function which will exicute next link
+    $('.next').on('click',function(){
+        // selecting `ul` elements and there direct descendants `li`
+        //  using the each method to interate over
+        $('ul > li').each(function (index, element) {
 
+        });
+    });
 });
