85dce3645f63a4ba15efe84d63f85c513d30f771
Brent <brentw.white@gmail.com>
2015-05-28 13:57:22 -1000

    logic done

--- js/javascript.js
+++ js/javascript.js
@@ -6,17 +6,18 @@ $(document).ready(function(){
         $('ul > li').each(function (index, element, number) {
             // first if statement
             //  if `element` hasClass('on') is true then use
-            // the `element` to access the `number` of the data-image attribute
-            if ($('element').hasClass('on')){
-                 number = $('element').data('image');
+            if ($('element').hasClass('on')) {
+                // the number variable to access the `element` in the data-image attribute
+                number = $('element').data('image')
+
             }
 
             // second if statement
             // first condition
             // if number of arguments is greater than 0 is`true`
             // second condition
-            // if `image_` + (number + 1) is `true`
-            if ($('image_' + (number + 1)).length > 0){
+            // if `image_` + (number + 1) is `true` then
+            if ($('image_' + (number + 1).length > 0)){
                 // next is assigned to number
                 // add number by `1` each time we iterate
                next = number + 1;
