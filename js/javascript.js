e162f8f82913fe27d841c863a9e7f89fe6af008b
Brent <brentw.white@gmail.com>
2015-06-08 15:28:35 -1000

    next and prev buttons working

--- js/javascript.js
+++ js/javascript.js
@@ -22,4 +22,28 @@ $(document).ready(function(){
             $('li.image_' + next).removeClass('off').addClass('on');
         });
     });
+    // event
+    $('.prev').on('click',function(){
+        // loop
+        $('ul > li').each(function (index,element){
+                // if element has a class of `on`
+                if ($(element).hasClass('on')){
+                    var number = $(element).data('image');
+                // if number of arguments is less than 0 and
+                // number - 1 is true then
+                if ($('.image_' + (number - 1)).length > 0){
+                    // prev is assigned to number and minused by 1
+                   prev = number - 1;
+                }
+                   else{
+                       next = 0;
+                   }
+            }
+
+    }).promise().done(function(){
+        // remove class `on` add class `off`
+        $('.carousel > ul > li').removeClass('on').addClass('off');
+        $('li.image_'+ prev).removeClass('off').addClass('on');
+        });
+    });
 });
